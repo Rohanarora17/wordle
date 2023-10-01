4a4e865a40e610e08240096374ef3c57dda6009c
Rohanarora17 <rohanarora494@gmail.com>
2023-10-02 00:02:58 +0530

    changes

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,7 +8,7 @@ import GuessResults from '../GuessResults/GuessResults';
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+
 
 function Game() {
   return(
